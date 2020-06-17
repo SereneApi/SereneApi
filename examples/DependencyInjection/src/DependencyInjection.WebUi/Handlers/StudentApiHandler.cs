441a6a3b395336e026a734eed4dc59eeffb416ab
Bradley Clarke <bradley.clarke@outlook.com>
2020-06-17 18:02:35 +1000

    UPDATED | Examples

--- examples/DependencyInjection/src/DependencyInjection.WebUi/Handlers/StudentApiHandler.cs
+++ examples/DependencyInjection/src/DependencyInjection.WebUi/Handlers/StudentApiHandler.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace DependencyInjection.WebUi.Handlers
 {
-    public class StudentApiHandler : ApiHandler, IStudentApi
+    public class StudentApiHandler: ApiHandler, IStudentApi
     {
         // This is important for Dependency Injection to work!
         // The Handler name must be set as the generic.
@@ -20,36 +20,39 @@ namespace DependencyInjection.WebUi.Handlers
         {
             // This GET request will use the students Id as a parameter for the request.
             // http://localhost:8080/api/Students/{studentId}
-            return InPathRequestAsync<StudentDto>(Method.Get, studentId);
+            return PerformRequestAsync<StudentDto>(Method.Get, r => r
+                .WithEndPoint(studentId));
         }
 
         public Task<IApiResponse<List<StudentDto>>> GetAllAsync()
         {
             // This is a simple GET request with no endpoint or parameters provided.
             // http://localhost:8080/api/Students
-            return InPathRequestAsync<List<StudentDto>>(Method.Get);
+            return PerformRequestAsync<List<StudentDto>>(Method.Get);
         }
 
         public Task<IApiResponse<List<StudentDto>>> FindByGivenAndLastName(StudentDto student)
         {
             // In this example, only the Given and Last name values will used for the query.
             // http://localhost:8080/api/Students?GivenName=value&LastName=value
-            return InPathRequestWithQueryAsync<List<StudentDto>, StudentDto>(Method.Get, student,
-                s => new { s.GivenName, s.LastName });
+            return PerformRequestAsync<List<StudentDto>>(Method.Get, r => r
+                .WithQuery(student, s => new { s.GivenName, s.LastName }));
         }
 
         public Task<IApiResponse> CreateAsync(StudentDto student)
         {
             // The StudentDto value will be passed to JSON and sent in the body of the request
             // http://localhost:8080/api/Students
-            return InBodyRequestAsync(Method.Post, student);
+            return PerformRequestAsync(Method.Post, r => r
+                .WithInBodyContent(student));
         }
 
         public Task<IApiResponse<List<ClassDto>>> GetStudentClasses(long studentId)
         {
             // Here we are using an Endpoint Template, allowing more complex APIs.
             // http://localhost:8080/api/Students/{studentId}/Classes
-            return InPathRequestAsync<List<ClassDto>>(Method.Get, "{0}/Classes", studentId);
+            return PerformRequestAsync<List<ClassDto>>(Method.Get, r => r
+                .WithEndPointTemplate("{0}/Classes", studentId));
         }
     }
 }
