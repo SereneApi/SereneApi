1545f950ad541fc8c4f989181fd191b10106cac9
Bradley Clarke <bradley.clarke@outlook.com>
2020-07-06 14:47:42 +1000

    ADDED | Newtonsoft

---
+++ src/SereneApi.Serialization.Newtonsoft/NewtonsoftDeserializer.cs
@@ -0,0 +1,73 @@
+﻿using Newtonsoft.Json;
+using SereneApi.Abstractions.Requests;
+using SereneApi.Abstractions.Requests.Content;
+using SereneApi.Abstractions.Serializers;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace SereneApi.Serialization.Newtonsoft
+{
+    public class NewtonsoftDeserializer: ISerializer
+    {
+        private readonly JsonSerializerSettings _deserializerOptions;
+
+        private readonly JsonSerializerSettings _serializerOptions;
+
+        public NewtonsoftDeserializer()
+        {
+            _deserializerOptions = DefaultDeserializerOptions;
+            _serializerOptions = DefaultSerializerOptions;
+        }
+
+        public NewtonsoftDeserializer(JsonSerializerSettings sharedOptions)
+        {
+            _deserializerOptions = sharedOptions;
+            _serializerOptions = sharedOptions;
+        }
+
+        public NewtonsoftDeserializer(JsonSerializerSettings deserializerOptions, JsonSerializerSettings serializerOptions)
+        {
+            _deserializerOptions = deserializerOptions;
+            _serializerOptions = serializerOptions;
+        }
+
+        public TObject Deserialize<TObject>(HttpContent content)
+        {
+            string contentString = content.ReadAsStringAsync().Result;
+
+            return JsonConvert.DeserializeObject<TObject>(contentString, _deserializerOptions);
+        }
+
+        public async Task<TObject> DeserializeAsync<TObject>(HttpContent content)
+        {
+            string contentString = await content.ReadAsStringAsync();
+
+            return JsonConvert.DeserializeObject<TObject>(contentString, _deserializerOptions);
+        }
+
+        public IApiRequestContent Serialize<TObject>(TObject value)
+        {
+            string jsonContent = JsonConvert.SerializeObject(value, _serializerOptions);
+
+            return new JsonContent(jsonContent);
+        }
+
+        public Task<IApiRequestContent> SerializeAsync<TObject>(TObject value)
+        {
+            return Task.Factory.StartNew(() =>
+            {
+                string jsonContent = JsonConvert.SerializeObject(value, _serializerOptions);
+
+                return (IApiRequestContent)new JsonContent(jsonContent);
+            });
+        }
+
+        public static JsonSerializerSettings DefaultDeserializerOptions = new JsonSerializerSettings
+        {
+        };
+
+        public static JsonSerializerSettings DefaultSerializerOptions = new JsonSerializerSettings
+        {
+        };
+    }
+}
