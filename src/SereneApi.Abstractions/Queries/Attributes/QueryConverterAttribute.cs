f1e94da907ace076d7a319f92671f1e394354710
Bradley Clarke <bradley.clarke@outlook.com>
2020-07-08 13:17:45 +1000

    FEAT | Query Attributes Implemented

---
+++ src/SereneApi.Abstractions/Queries/Attributes/QueryConverterAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SereneApi.Abstractions.Queries.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class QueryConverterAttribute: Attribute
+    {
+        public IQueryConverter Converter { get; }
+
+        public QueryConverterAttribute(Type converter)
+        {
+            Converter = (IQueryConverter)Activator.CreateInstance(converter);
+        }
+    }
+}
---
+++ src/SereneApi.Abstractions/Queries/Exceptions/QueryRequiredException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace SereneApi.Abstractions.Queries.Exceptions
+{
+    public class QueryRequiredException: Exception
+    {
+        public QueryRequiredException(string propertyName) : base($"{propertyName} must be provided as it is a required part of the query", new ArgumentNullException(propertyName))
+        {
+        }
+    }
+}
