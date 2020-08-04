27dca690ee959685170e72725f3e72d77fd99611
Bradley Clarke <bradley.clarke@outlook.com>
2020-08-04 12:35:04 +1000

    ADDED | DependencyInjection Tests

---
+++ tests/SereneApi.DependencyInjection.Tests/ITestApi.cs
@@ -0,0 +1,10 @@
+﻿using SereneApi.Abstractions.Handler;
+using SereneApi.Abstractions.Options;
+
+namespace SereneApi.DependencyInjection.Tests
+{
+    public interface ITestApi: IApiHandler
+    {
+        IApiOptions Options { get; }
+    }
+}
