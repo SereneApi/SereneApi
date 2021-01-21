400e9dc3d3b14a3218545a6d31e16f362944710b
Bradley Clarke <bradley.clarke@outlook.com>
2021-01-21 11:23:44 +1100

    CODE CLEANUP

--- src/SereneApi.Abstractions/Options/ApiOptionsBuilder.cs
+++ src/SereneApi.Abstractions/Options/ApiOptionsBuilder.cs
@@ -1,6 +1,5 @@
 ﻿using DeltaWare.Dependencies;
 using DeltaWare.Dependencies.Abstractions;
-using Microsoft.Extensions.Logging;
 using SereneApi.Abstractions.Authorization;
 using SereneApi.Abstractions.Authorization.Types;
 using SereneApi.Abstractions.Configuration;
