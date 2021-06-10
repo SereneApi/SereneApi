196576ddbc168932624eb858f0992846b26e9ef0
Bradley Clarke <bradley.clarke@outlook.com>
2021-06-10 13:56:44 +1000

    Updated | AspNet Benchmark

--- benchmarks/SereneApi.Benchmark.AspNet/API/StudentApiHandler.cs
+++ benchmarks/SereneApi.Benchmark.AspNet/API/StudentApiHandler.cs
@@ -1,11 +1,11 @@
 ﻿using SereneApi.Abstractions.Options;
-using SereneApi.Abstractions.Request;
+using SereneApi.Abstractions.Requests;
 using SereneApi.Abstractions.Response;
 using System.Threading.Tasks;
 
 namespace SereneApi.Benchmark.AspNet.API
 {
-    public class StudentApiHandler: BaseApiHandler, IStudentApi
+    public class StudentApiHandler : BaseApiHandler, IStudentApi
     {
         public StudentApiHandler(IApiOptions<IStudentApi> options) : base(options)
         {
@@ -13,7 +13,10 @@ namespace SereneApi.Benchmark.AspNet.API
 
         public Task<IApiResponse<StudentDto>> GetStudents()
         {
-            return PerformRequestAsync<StudentDto>(Method.GET);
+            return BuildRequest
+                .UsingMethod(Method.GET)
+                .RespondsWithContent<StudentDto>()
+                .ExecuteAsync();
         }
     }
 }
